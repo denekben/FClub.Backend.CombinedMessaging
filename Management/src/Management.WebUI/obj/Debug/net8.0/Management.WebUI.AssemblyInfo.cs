49f6185701bf37a9c5576400ac851472d6234f05
denekben <denekben1@yandex.ru>
2025-04-22 18:27:04 +0300

    Updated FClub.Backend.Common

--- Management/src/Management.WebUI/obj/Debug/net8.0/Management.WebUI.AssemblyInfo.cs
+++ Management/src/Management.WebUI/obj/Debug/net8.0/Management.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Management.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d8fb2ab9d1fb535452ec5dae2e31bcab987d34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c309ff48b8f374266708ed1522aabf501f69a292")]
 [assembly: System.Reflection.AssemblyProductAttribute("Management.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Management.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
